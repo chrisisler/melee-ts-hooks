53b497b788909a907947c59e83560c2980145467
Chris Isler <christopherisler1@gmail.com>
2019-08-22 13:18:20 -0400

    Add simple HTML structure

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,40 @@
 import React, { FC } from 'react';
 import { render } from 'react-dom';
-import './index.css';
+import './index.scss';
 import * as serviceWorker from './serviceWorker';
+import { Switch, Route, BrowserRouter as Router } from 'react-router-dom';
 
-const App: FC = () => {
-    return (
-        <div>
-            lmao
-            </div>
-    );
-}
+const Header: FC = () => (
+  <Switch>
+    <Route
+      render={() => {
+        // XXX Show character name as header title.
+        return (
+          <header>
+            <h3>SSBM Frames</h3>
+          </header>
+        );
+      }}
+    />
+  </Switch>
+);
 
-render(<App />, document.getElementById('root'));
+const Page: FC = () => {
+  return (
+    <main>
+      <h2>Melee frame data web-app built using React Hooks and Typescript</h2>
+    </main>
+  );
+};
+
+const Application: FC = () => (
+  <Router>
+    <Header />
+    <Route component={Page} />
+  </Router>
+);
+
+render(<Application />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
